ab2cb44591983785820ec1e533d2f82844ce9985
RoHiT-engi <79554502+RoHiT-engi@users.noreply.github.com>
2021-03-30 21:55:29 +0530

    Add files via upload

---
+++ Drive.py
@@ -0,0 +1,117 @@
+import pickle
+import os.path
+from typing import List, Any
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaFileUpload
+from tkinter import messagebox
+from tkinter import *
+import os
+
+class MyDrive():
+    thisList = []
+
+    def __init__(self):
+        # If modifying these scopes, delete the file token.pickle.
+        SCOPES = ['https://www.googleapis.com/auth/drive']
+        """Shows basic usage of the Drive v3 API.
+        Prints the names and ids of the first 10 files the user has access to.
+        """
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        self.service = build('drive', 'v3', credentials=creds)
+
+    def list_files(self, folder_id):
+        # Call the Drive v3 API
+
+        query = f"parents= '{folder_id}'"
+        results = self.service.files().list(
+             q=query,
+             fields="nextPageToken, files(id, name)").execute()
+        items = results.get('files', [])
+        listing = Toplevel()
+        listing.title('List of Files')
+        listing.resizable(width=True, height=True)
+
+        if not items:
+            print('No files found.')
+            messagebox.showinfo('No File Found', 'No Files in Backup folder')
+        else:
+            print('Files:')
+            for item in items:
+                a = u'{0} ({1})'.format(item['name'], item['id'])
+                lbl = Label(listing, text=a)
+                lbl.pack()
+            Button(listing, text='OK', command=listing.destroy).pack()
+        listing.mainloop()
+
+
+
+    def upload_file(self, filename, path, folder_id):
+        media = MediaFileUpload(f"{path}/{filename}", resumable=True)
+
+        response = self.service.files().list(
+                                        q=f"name='{filename}' and parents='{folder_id}'",
+                                        spaces='drive',
+                                        fields='nextPageToken, files(id, name)',
+                                        pageToken=None).execute()
+        if len(response['files']) == 0:
+            file_metadata = {
+                'name': filename,
+                'parents': [folder_id]
+            }
+            file = self.service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+            print(f"A new file was created {file.get('id')}")
+
+        else:
+            for file in response.get('files', []):
+                # Process change
+
+                update_file = self.service.files().update(
+                    fileId=file.get('id'),
+                    media_body=media,
+                ).execute()
+                print(f'Updated File')
+
+
+def main1(file_path,folder_id,request,zip_File_name):
+    files = os.listdir(file_path)
+    my_drive = MyDrive()
+
+    if(request == 'upload'):
+        if (folder_id!=""):
+            MyDrive.thisList.append(folder_id)
+        for item in files:
+            my_drive.upload_file(item, file_path, MyDrive.thisList[len(MyDrive.thisList)-1])
+        messagebox.showinfo('upload status', 'your file is uploaded to Drive succesfully')
+    elif(request == 'list_files'):
+        my_drive.list_files(MyDrive.thisList[len(MyDrive.thisList)-1])
+    else:
+        if (folder_id!=""):
+            MyDrive.thisList.append(folder_id)
+        for item in files:
+            if(item == zip_File_name):
+                my_drive.upload_file(item, file_path, MyDrive.thisList[len(MyDrive.thisList)-1])
+        messagebox.showinfo('upload status', 'your file is uploaded to Drive succesfully')
+
+if __name__ == '__main__':
+    main1()
--- source.py
+++ source.py
@@ -1,20 +1,19 @@
+import os
 from tkinter import *
 from tkinter import messagebox
-from subprocess import call
-
+from Drive import *
 from convert_to_zip import *
+from typing import Final
 options = Tk()
 options.title("Home Page")
-#uploading_to_drive = call(["uploading_to_drive","C:/Users/91922/PycharmProjects/file_manager/uploading _to_Drive.py"])
 # options frame
 menu_frame = LabelFrame(options, text="Chose The Following", padx=100, pady=100)
 menu_frame.pack(padx=50, pady=50)
 
 login_status = False
 
-
 def loggin():
-    #loggin window
+    # loggin window
     login = Toplevel()
     login.title('File Manager & Recovery System')
     login.geometry('500x500')
@@ -102,41 +101,88 @@ def window32():
             uploading = Toplevel()
             uploading.title('Taking Path & ID')
             uploading.geometry('400x100')
-            backups.destroy()
-            #folder_id = ""
-            taking_ID = Label(uploading, text="Enter your Drive File ID ").pack()
-            folder_id = Entry(uploading, width=40)
-            folder_id.pack(pady=5)
+            ask_first_time_login = messagebox.askquestion('ask first time', 'Did you provide destination Folder ID?')
+            if ask_first_time_login == 'no':
+                messagebox.showinfo('change the id request', 'plz provide a destination id')
+                Label(uploading, text="Enter your Drive File ID ").pack()
+                folder_id = Entry(uploading, width=40)
+                folder_id.pack(pady=5)
+                def first_id():
+                    main1(str(take_path), folder_id.get(), 'upload', "")
+                    uploading.destroy()
+                Button(uploading, text="Click to Continue",command=first_id).pack()
+            else:
+                ask_destination = messagebox.askquestion('taking destination', 'change the destination Id?')
+                if ask_destination == 'yes':
+                    Label(uploading, text="Enter your Drive File ID ").pack()
+                    folder_id = Entry(uploading, width=40)
+                    folder_id.pack(pady=5)
+                    def change_id():
+                        main1(str(take_path), str(folder_id.get()), 'upload',"")
+                        uploading.destroy()
+                    Button(uploading, text="Click to Continue", command= change_id).pack()
+                    #uploading.destroy()
+                else:
+                    #Label(uploading, text=str(fixed_id)+'hii').pack()
+                    main1(str(take_path), "", 'upload', "")
+                    uploading.destroy()
 
-            Button(uploading, text="Enter").pack()
         def zipping(take_path):
             zipp =Toplevel()
             zipp.title("zip a file")
             zipp.geometry('400x250')
-            backups.destroy()
             Label(zipp, text="Enter ZIP File Name").pack()
             file_name = Entry(zipp, width=50)
             file_name.pack(pady=5)
 
             def zip_fun(path_passing):
                 main(str(path_passing), str(file_name.get()))
-                zipp.destroy()
+                give_path = os.getcwd()
+                path = f"{file_name.get()}" + ".zip"
+                uploading = Toplevel()
+                uploading.title('Taking Path & ID')
+                uploading.geometry('400x100')
+                ask_first_time_login = messagebox.askquestion('ask first time', 'Did you provide destination Folder ID?')
+                if ask_first_time_login == 'no':
+                    messagebox.showinfo('change the id request', 'plz provide a destination id')
+                    Label(uploading, text="Enter your Drive File ID ").pack()
+                    folder_id = Entry(uploading, width=40)
+                    folder_id.pack(pady=5)
+                    def first_id():
+                        main1(str(give_path), folder_id.get(), '', path)
+                        uploading.destroy()
+                        zipp.destroy()
+                    Button(uploading, text="Click to Continue", command=first_id).pack()
+                else :
+                    main1(str(give_path), "", '', path)
+                    uploading.destroy()
+                    zipp.destroy()
             Button(zipp, text="Enter", command= lambda :zip_fun(take_path)).pack()
+
+        def list_files(take_path):
+            main1(take_path, "", "list_files","")
 
         backup_frame = LabelFrame(backups, text="Create a Backup", padx=100, pady=100)
         backup_frame.pack(padx=50, pady=50)
         Button(backup_frame, text="Upload to Google Drive", command=lambda : upload(path)).pack()
-        Button(backup_frame, text="convert Backup File to ZIP",command= lambda :zipping(path)).pack()
+        Button(backup_frame, text="convert Backup File to ZIP & upload to Drive",command= lambda :zipping(path)).pack()
         Button(backup_frame, text="Download from Drive").pack()
-        Button(backup_frame, text="files in Google Drive backup").pack()
+        Button(backup_frame, text="files in Google Drive backup", command=lambda :list_files(path)).pack()
+        Button(backup_frame, text="back to Home Page", command=backups.destroy).pack()
+
+
     Label(takingdirc_path,text="Enter the Backup folder Path").pack()
     backup_path = Entry(takingdirc_path, width=50)
     backup_path.pack()
     Button(takingdirc_path,text="Enter",command=lambda :taking_path(backup_path.get())).pack()
 
+def logout():
+    messagebox.showinfo('Logging Out','Thank You for using File Manager')
+    options.destroy()
+
 
 blog = Button(menu_frame, text="Login", command=loggin).grid(row=0, column=0)
 b1 = Button(menu_frame, text="Work on File", command= window31).grid(row=1, column=0)
 b2 = Button(menu_frame, text="Make A Backup", command= window32).grid(row=2, column=0)
-b3 = Button(menu_frame, text="Exit", command=options.destroy).grid(row=3, column=0)
+b3 = Button(menu_frame, text="Log Out", command=logout).grid(row=3, column=0)
 options.mainloop()
