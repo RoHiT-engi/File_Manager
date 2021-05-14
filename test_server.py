03ea81c92f201094d0cd38072b03d67b91411ff5
RoHiT-engi <79554502+RoHiT-engi@users.noreply.github.com>
2021-05-14 11:31:44 +0530

    Delete test_server.py

--- test_server.py
+++
@@ -1,9 +0,0 @@
-import socket
-Headersize = 10
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind((socket.gethostname(),5421))
-s.listen(5)
-while True:
-    clientsocket,address = s.accept()
-    print(f'Connection from {address} has started')
-    clientsocket.send(bytes("welcome to server its ME ",'utf-8'))
